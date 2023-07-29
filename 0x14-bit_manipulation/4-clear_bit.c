2a41aaa781f35ca85cfa4a5573ab3f3b897da0e5
peter <01peterkinyanjui@gmail.com>
2023-07-29 10:21:26 -0700

    task4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,7 +7,7 @@
  * This function clears the bit at the specified index in the given number.
  * Return: 1 if it worked (bit cleared successfully),
  */
-clear_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int bitmask;
 /* Check if the index is valid*/
