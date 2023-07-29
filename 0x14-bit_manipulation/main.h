f019438737f660d753cb1b94bb2ad5e1eb8a721e
peter <01peterkinyanjui@gmail.com>
2023-07-29 08:58:43 -0700

    c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,6 +9,8 @@
 unsigned long int _power(unsigned int base, unsigned int pow);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+char *_memset_custom(char *memory, char value, unsigned int bytes);
+void *_calloc_custom(unsigned int num_elements, unsigned int element_size);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
