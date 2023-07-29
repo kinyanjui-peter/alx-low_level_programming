085c40f9eb3d8601275a848e25f59df02c65d9d9
peter <01peterkinyanjui@gmail.com>
2023-07-29 09:36:05 -0700

    task2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,22 +10,17 @@
  */
 int get_bit(unsigned long int num, unsigned int bit_index)
 {
-    unsigned long int bitmask, bit_value;
+	unsigned long int bitmask, bit_value;
 
-    // Check if the bit_index is within the valid range for the number
-    if (bit_index > (sizeof(unsigned long int) * 8 - 1))
-        return (-1);
+	if (bit_index > (sizeof(unsigned long int) * 8 - 1))
+	return (-1);
 
-    // Create a bitmask with only the bit at bit_index set to 1
-    bitmask = 1 << bit_index;
+	bitmask = 1 << bit_index;
+	bit_value = num & bitmask;
 
-    // Use bitwise AND to check if the bit at bit_index is set to 1
-    bit_value = num & bitmask;
-
-    // If the bit is set, return 1; otherwise, return 0
-    if (bit_value == bitmask)
-        return (1);
-    else
-        return (0);
+	if (bit_value == bitmask)
+	return (1);
+	else
+	return (0);
 }
 
