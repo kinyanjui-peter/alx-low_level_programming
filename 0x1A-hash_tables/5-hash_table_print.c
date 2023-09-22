108c6ee1656e4bfacfc1646628ee2259b41ff594
peter <01peterkinyanjui@gmail.com>
2023-09-22 00:32:34 -0700

    task5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 /**
- * hash_table_print - function that prints a hash table
+ * hash_table_print - function name that prints a hash table
  * @ht: is the hash table
  * Return - key/value or NULL
  *
