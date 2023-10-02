cee8c62622c4873822abd2b5fc85d87bf15d7d9e
peter <01peterkinyanjui@gmail.com>
2023-10-02 03:36:34 -0700

    task1

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,25 +7,25 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    hash_node_t *temp, *del_node;
-    hash_table_t *first = ht;
-    unsigned long int counter;
+	hash_node_t *temp, *del_node;
+	hash_table_t *first = ht;
+	unsigned long int counter;
 
-    for (counter = 0; counter < ht->size; counter++)
-    {
-        if (ht->array[counter] != NULL)
-        {
-            del_node = ht->array[counter];
-            while (del_node != NULL)
-            {
-                temp = del_node->next;
-                free(del_node->key);
-                free(del_node->value);
-                free(del_node);
-                del_node = temp;
-            }
-        }
-    }
-    free(first->array);
-    free(first);
+	for (counter = 0; counter < ht->size; counter++)
+	{
+	if (ht->array[counter] != NULL)
+	{
+		del_node = ht->array[counter];
+		while (del_node != NULL)
+		{
+			temp = del_node->next;
+			free(del_node->key);
+			free(del_node->value);
+			free(del_node);
+			del_node = temp;
+		}
+	}
+}
+free(first->array);
+free(first);
 }
